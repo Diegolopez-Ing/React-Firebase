da2a178497721de5a46eafffd8ca6d07d6b66ed4
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-16 14:43:42 -0500

    Pruebass con JSX

--- primerproyecto/src/components/Hola.jsx
+++ primerproyecto/src/components/Hola.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React from/*,{Fragment}*/ 'react';
 
 function Hola() {
-    return (
+    let saludo='Hola Diego!!!'
+    return (//Se puede hacer uso de {Fragment} en lugar de {div} para encerrar codigos
         <div>
-            <h2>Primer Componente !!!</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam unde, quidem laborum perferendis quasi, facere, repudiandae architecto voluptas nisi nemo officiis ex harum molestias necessitatibus. Dolores corporis dicta incidunt amet.</p>
+            <h2>Saluden a Diego, {saludo}{2*3}</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, eos doloremque delectus provident unde quaerat. Amet iure aliquid distinctio quisquam sint nemo similique laudantium deleniti voluptates ad! Minima, quibusdam minus.</p>
         </div>)
 }
 export default Hola
