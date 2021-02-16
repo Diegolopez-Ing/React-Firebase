fabfd2253275fbb954244a755db0deeb0b7b7007
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-16 11:24:35 -0500

    Declaracion de variables y tipos

---
+++ Nivelacion/variables.js
@@ -0,0 +1,25 @@
+//Declaraciones 
+var nombreVariable = 'string'//, functions, booleanos,array, numeros,objetos
+
+///Actualment
+
+let nombre = 'Diego'//Let se usa para variables mutables
+console.log(nombre);
+nombre = 43
+console.log(nombre);
+
+const prueba2 = 'Diego' //const inmutable}
+console.log(prueba2);
+// prueba2=9
+
+const coleccion = [1, 2, 3, 45, 5]
+console.log(coleccion);
+
+
+const objeto = {
+    nombre: 'diego',
+    apellido: 'Lopez',
+    edad: 25
+}
+
+console.log(objeto);
