c518669c6fb9877d88e6e40d12083fc84cf4c4da
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-16 15:07:55 -0500

    UseState

--- primerproyecto/src/App.jsx
+++ primerproyecto/src/App.jsx
@@ -1,11 +1,17 @@
+import { useState } from 'react';
 import './App.css';
-import Hola from './components/Hola'
+// import Hola from './components/Hola'
+import Contador from './components/contador'
 
 function App() {
+
+ 
   return (
     <div className="App">
       <h1>Nuestro primer Proyecto</h1>
-      <Hola></Hola>
+      {/* <Hola></Hola> */}
+      <Contador></Contador>
+      <Contador></Contador>
     </div>
   );
 }
