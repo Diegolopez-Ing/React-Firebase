a68290060bdc89ce8ce95bf3ff580ca9908efb6c
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-16 18:00:23 -0500

    Operador de Propagacion

---
+++ Nivelacion/propagacion.js
@@ -0,0 +1,7 @@
+const array1=[1,2,3,8]
+const array2=[4,5,2,6]
+
+
+// ... Esto tres puntos significas operador de propagación(ejecutar quokka para ver diferencia)
+const array3=[...array1,...array2]
+console.log(array3);
