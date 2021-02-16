ad022d4b3f3ac41ca7e9937a71dcb8c1cb8e513d
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-16 14:32:55 -0500

    Creacion del primer componente Hola

---
+++ primerproyecto/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Hola from './components/Hola'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Nuestro primer Proyecto</h1>
+      <Hola></Hola>
+    </div>
+  );
+}
+
+export default App;
---
+++ primerproyecto/src/components/Hola.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Hola() {
+    return (
+        <div>
+            <h2>Primer Componente !!!</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam unde, quidem laborum perferendis quasi, facere, repudiandae architecto voluptas nisi nemo officiis ex harum molestias necessitatibus. Dolores corporis dicta incidunt amet.</p>
+        </div>)
+}
+export default Hola
