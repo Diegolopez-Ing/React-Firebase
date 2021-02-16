4c2350bcad6043a815351d2bb99b364d76155d36
Diegolopez-ing <diegolopez.coder@gmail.com>
2021-02-16 11:31:30 -0500

    Templates String ` `

---
+++ Nivelacion/templates.js
@@ -0,0 +1,8 @@
+const nombre='Maria'
+const apellido='Palacios'
+
+console.log('EL nombre es '+ nombre+ ' y el apellido es '+apellido);
+
+//TTEMPLATES STRING   ` `
+console.log(`El nombre es ${nombre} y el apellido es ${apellido}`);
+
